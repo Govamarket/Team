b19e4e5f11ff747c4580146afef165a7dc51a6fb
Gova <govaway@gmail.com>
2023-03-02 18:47:12 +0100

    <<Movie landing page not yet completed>>

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
-    const container = document.querySelector(".container");
 
     // the toggle navigation
     burger.addEventListener("click", () => {
@@ -11,7 +10,6 @@ const navSlide = () => {
       navLinks.forEach((link, index) => {
         if (link.style.animation) {
           link.style.animation = "";
-          container.style.visibility = "visible";
           link.style.animation = `navLinksFade 0.5s ease forwards ${
             index / 7 + 1.5
           }s`;
